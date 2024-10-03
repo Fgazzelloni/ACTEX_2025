f7a31b0f4b88a61e304959832c0f6dac1a89a668
fgazzelloni <docksbox@pm.me>
2024-10-03 14:41:19 +0200

    updating material

---
+++ 00_helper.R
@@ -0,0 +1,31 @@
+# List both base and recommended packages
+installed.packages(priority = c("base", "recommended"))
+
+search()
+?search()
+
+# Install the necessary packages
+# Packages can be installed using the install.packages() function
+# if the package is on CRAN (Comprehensive R Archive Network)
+# otherwise use devtools::install_github("username/package_name")
+install.packages("tidyverse")
+
+# Load the necessary libraries
+library(tidyverse)
+
+# To find data sets in R
+data()
+
+# To find out more about an object in R
+?...
+?data()
+
+# To find help
+help.start()
+help()
+
+# To list the objects in the environment
+ls()
+
+# To remove all objects from the environment
+rm(list = ls())
